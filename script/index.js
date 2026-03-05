454ac929135b032278520ef28c51f773d2aab84e
sanoyon211 <sanoyon522@gmail.com>
2026-03-05 11:48:48 +0600

    Accordion section and footer section done

--- script/index.js
+++ script/index.js
@@ -3,6 +3,15 @@ const createElement = (arr) => {
   return htmlElements.join(" ");
 }
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = 'en-EN'; // English
+  utterance.rate = 0.7;
+  utterance.pitch = 1.2;
+  window.speechSynthesis.speak(utterance);
+}
+
+
 const manageSpinner = (status) => {
   if (status == true) {
     document.getElementById('spinner').classList.remove("hidden");
@@ -121,7 +130,7 @@ const displayLevelWord = words => {
           <div class="font-bangla text-lg md:text-2xl font-medium">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায়নি'}"</div>
           <div class="flex justify-between items-center">
             <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF]/80"><i class="fa-solid fa-circle-info"></i></button>
-            <button class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF]/80"><i class="fa-solid fa-volume-high"></i></button>
+            <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF]/10 hover:bg-[#1A91FF]/80"><i class="fa-solid fa-volume-high"></i></button>
           </div>
         </div>`;
     wordContainer.appendChild(card);
